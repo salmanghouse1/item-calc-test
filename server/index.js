e5ff5028cf6f891a55a49281ab667798530bc353
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-05-09 13:13:30 -0400

    added backend api routes-in progress

--- server/index.js
+++ server/index.js
@@ -12,3 +12,17 @@ const db = mysql.createPool({
 });
 
 app.use(cors());
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hi There, api test");
+});
+
+
+// get data from api rest api
+app.get("/api/getData", (req, res) => {
+  
+});
