c8f4fbd2a4a714bfa11fdf99a93572febbb48bd5
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-05-09 13:06:39 -0400

    added mysql cred

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  // the sql data will be retrieved from docker file in the future.
+  host: process.env.DB_HOST, // the host name MYSQL_DATABASE: node_mysql
+  user: process.env.DB_USER, // database user MYSQL_USER: MYSQL_USER
+  password: process.env.DB_PASS, // database user password MYSQL_PASSWORD: MYSQL_PASSWORD
+  database: "user_products_db", // database name MYSQL_HOST_IP: mysql_db
+});
+
+app.use(cors());
